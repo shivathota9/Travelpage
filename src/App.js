c7f4a8e22f9a275732daf1d291fbab639c484747
shivathota9 <thotashiva9@gmail.com>
2024-02-08 23:50:22 +0530

    sol

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+import Gallery from './Gallery';
 
-import { BrowserRouter,Route, Routes } from  "react-router-dom"
-
-import Home from './Home'
-import About from './About'
-import Details from './Details'
-
-
+const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
 const App = () => {
+  const [data,setData] = useState([]);
+  const [search,setSearch] = useState("");
+  useEffect(()=>{
+    },[])
+  const changeHandler = e =>{
+    setSearch(e.target.value);
+  }
+  const submitHandler = e =>{
+    e.preventDefault();
+    axios
+    .get(
+      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`
+    )
+    .then(response => {
+      setData(response.data.photos.photo)
+    })
+    .catch(error => {
+      console.log(
+        "Encountered an error with fetching and parsing data",
+        error
+      );
+  })
+  }
   return (
-    <BrowserRouter> 
-     <Navbar/>
-      <Routes>
-        < Route path="/" exact component = {Home} />
-        < Route path="/details" exact component ={Details}/>
-        < Route path="/about" exact component = {About} />
-      </Routes>
-    </BrowserRouter>
-
+    <div>
+      <center>
+        <h2>Gallery Snapshot</h2><br></br>
+        <form onSubmit={submitHandler}>
+          <input size="30" type="text" onChange={changeHandler} value={search}/><br /><br />
+          <input type="submit" name="Search" />
+        </form>
+        <br />
+        {data.length>=1?<Gallery data={data}/>:<h4>No Image Loaded</h4>}
+      </center>
+    </div>
   )
 }
 
