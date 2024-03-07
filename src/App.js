4762bbca566c6d67ec00feefa165008ba5dec0bf
shivathota9 <thotashiva9@gmail.com>
2024-03-07 16:35:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React,{useState} from 'react'
-import Products from './Products';
+import React from 'react'
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import Signup from './components/pages/Signup'
+import Products from './components/pages/Products';
+
+
 
 const App = () => {
-  const [search,setSearch] = useState('');
-  const [data,setData] = useState([]);
-  const YOUR_APP_ID = "8aecf864";
-  const YOUR_APP_KEY ="b460ca299374805ea2ad4be4f6ea081a";
-  const submitHandler = e =>{
-    e.preventDefault();
-    fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`).then(
-      response => response.json()
-    ).then(
-      data => console.log(data)
-    )
-  }
   return (
-    <div>
-      <center>
-        <h4>Food Recipe App</h4>
-        <form onSubmit={submitHandler}>
-          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/> <br/>
-          <input type="submit" className="btn btn-primary" value="Search"/>
-        </form>
-        {data.length>=1 ? <Products  data={data}/>:null}
-      </center>
-    </div>
+    <>
+     <Router>
+       <Navbar/>
+      <Routes>
+       <Route path='/' exact Component={Home} />
+       <Route path='/services' exact Component={Services} />
+       <Route path='/products' exact Component={Products} />
+       <Route path='/sign-up' exact Component={Signup} />
+      </Routes>
+     </Router>
+     </>
   )
 }
 
